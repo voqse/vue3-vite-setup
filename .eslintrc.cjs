77a04e024429a597bc6eed9d0e3f7274bc934244
voqse <git@voqse.com>
2022-03-27 14:31:13 +0200

    Override conflicting ESLint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,5 +12,29 @@ module.exports = {
     semi: ['error', 'never'],
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'import/no-extraneous-dependencies': ['error', { devDependencies: ['*.{js,jsx,cjs,mjs}'] }],
   },
+  overrides: [
+    {
+      files: ['*.vue'],
+      rules: {
+        'max-len': 'off', // disables line length check
+        'vue/max-len': ['warn', {
+          code: 100,
+          // template: 100,
+          // tabWidth: 2,
+          // comments: 100,
+          // ignorePattern: '',
+          // ignoreComments: true,
+          // ignoreTrailingComments: true,
+          ignoreUrls: true,
+          ignoreStrings: true,
+          ignoreTemplateLiterals: true,
+          ignoreRegExpLiterals: true,
+          ignoreHTMLAttributeValues: true,
+          ignoreHTMLTextContents: true,
+        }],
+      },
+    },
+  ],
 }
