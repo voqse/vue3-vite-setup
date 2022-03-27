573918d999f26810f2052bc83fe2ce4c8aadc9dd
voqse <git@voqse.com>
2022-03-27 13:01:47 +0200

    Add eslint-config-airbnb

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,16 @@
 /* eslint-env node */
 module.exports = {
-  "root": true,
-  "extends": [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended"
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    '@vue/airbnb',
   ],
-  "env": {
-    "vue/setup-compiler-macros": true
-  }
+  env: {
+    'vue/setup-compiler-macros': true,
+  },
+  rules: {
+    semi: ['error', 'never'],
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+  },
 }
